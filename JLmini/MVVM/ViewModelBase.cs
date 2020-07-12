690c80ab0632505a7f817c287494aa1beaf5943c
Konstantin <heeleagle61@gmail.com>
2020-07-12 18:18:32 +0300

    Implement MVVM pattern

---
+++ JLmini/MVVM/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLmini.MVVM
+{
+    class VewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
