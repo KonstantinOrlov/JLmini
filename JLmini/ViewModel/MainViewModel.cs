07f69b333f8992900b0dd7038932cc3167647e78
Konstantin <heeleagle61@gmail.com>
2020-07-12 18:36:15 +0300

    Create MainViewModel

---
+++ JLmini/ViewModel/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using JLmini.Model;
+using JLmini.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace JLmini.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase currentViewModel;
+
+        PersonViewModel PersonVM = new PersonViewModel();
+        QuestionViewModel QuestionVM = new QuestionViewModel();
+
+        public ViewModelBase CurrentViewModel
+        {
+            get { return currentViewModel; }
+            set
+            {
+                currentViewModel = value;
+                OnPropertyChanged("CurrentViewModel");
+            }
+        }
+
+        public MainViewModel()
+        {
+            CurrentViewModel = PersonVM;
+        }
+    }
+}
